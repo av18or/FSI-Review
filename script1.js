e063570d1b33b11e564d06c36b989419d1bd7828
Clayton Gorham <claytongorham@gmail.com>
2021-11-20 19:31:03 -0800

    style and function updates

--- script1.js
+++ script1.js
@@ -4,18 +4,21 @@
 
 
 // gets items from local storage for 'dogVotes'
+let dogVotesTd = document.querySelector("#doggo-votes")
 let dogVotes = localStorage.getItem('dogVotes');
 if (dogVotes === null) {
     dogVotes = 0;
 } else {
     dogVotes = parseInt(dogVotes)
 }  
+
 document.querySelector("#doggo-votes").textContent = dogVotes;
 
 
 
 
 // gets items from local storage for 'catVotes'
+let catVotesTd = document.querySelector("#catto-votes")
 let catVotes = localStorage.getItem('catVotes');
 if (catVotes === null) {
     catVotes = 0;
@@ -28,6 +31,7 @@ document.querySelector("#catto-votes").textContent = catVotes;
 
 
 // gets items from local storage for 'fishVotes'
+let fishVotesTd = document.querySelector("#fish-votes")
 let fishVotes = localStorage.getItem('fishVotes');
 if (fishVotes === null) {
     fishVotes = 0;
@@ -38,8 +42,8 @@ document.querySelector("#fish-votes").textContent = fishVotes;
 
 
 ///////
-
-
+// ties in total vote column
+let totalVotesTd = document.querySelector("#total-votes");
 
 // selectors for each individual animal div 
 let dogDiv = document.querySelector("#doggo");
@@ -49,11 +53,12 @@ let fishDiv = document.querySelector("#fish");
 
 
 
+
 ///////
 
 // creating and append our dog vote button
 let dogVoteBtn = document.createElement('button');
-dogVoteBtn.textContent = "Vote for Doggo";
+dogVoteBtn.textContent = "+ Vote for Doggo";
 dogDiv.append(dogVoteBtn);
 
 
@@ -63,36 +68,76 @@ dogDiv.append(dogVoteBtn);
 dogVoteBtn.addEventListener('click', function(){
     dogVotes = dogVotes + 1;
     document.querySelector("#doggo-votes").textContent = dogVotes;
-    //sets the item in local storage, then we retrieve it AT TOP OF this PAGE using getItem. 
+//sets the item in local storage, then we retrieve it AT TOP OF this PAGE using getItem. 
     localStorage.setItem('dogVotes', dogVotes);
 
-    
+// adds to total vote column by adding all three together
+    totalVotesTd.textContent = dogVotes + catVotes + fishVotes;
 })
 
-///////
+
+// create dog vote MINUS button
+let SubdogVoteBtn = document.createElement("button")
+SubdogVoteBtn.textContent = "- Vote for Doggo"
+dogDiv.append(SubdogVoteBtn)
+
+SubdogVoteBtn.addEventListener('click', function(e) {
+        dogVotes = dogVotes - 1;
+        dogVotesTd.textContent = dogVotes;
+        localStorage.setItem('dogVotes', dogVotes);
+        
+        totalVotesTd.textContent = dogVotes + catVotes + fishVotes
+
+       
+});
+
+
+
+//
+
 
 // create and append our cat vote button
 let catVoteBtn = document.createElement('button');
-catVoteBtn.textContent = "Vote for Catto";
+catVoteBtn.textContent = "+ Vote for Catto";
 catDiv.append(catVoteBtn);
-
 
 // add event listener to our cat vote button, and update #catto-votes with each click.
 catVoteBtn.addEventListener('click', function(){
     catVotes = catVotes + 1;
     document.querySelector("#catto-votes").textContent = catVotes;
-    //sets the item in local storage, then we retrieve it AT TOP OF PAGE using getItem. 
+    
+//sets the item in local storage, then we retrieve it AT TOP OF PAGE using getItem. 
     localStorage.setItem('catVotes', catVotes);
 
-    
+
+// adds to total vote column by adding all three together
+    totalVotesTd.textContent = dogVotes + catVotes + fishVotes;
+
 })
 
 
-///////
+// create cat vote MINUS button
+let SubCatVoteBtn = document.createElement("button")
+SubCatVoteBtn.textContent = "- Vote for Catto"
+catDiv.append(SubCatVoteBtn)
+
+SubCatVoteBtn.addEventListener('click', function(e) {
+        catVotes = catVotes - 1;
+        catVotesTd.textContent = catVotes;
+        localStorage.setItem('catVotes', catVotes);
+// makes sure total vote count is also subtracted from
+        totalVotesTd.textContent = dogVotes + catVotes + fishVotes;
+
+});     
+
+
+
+//
+
 
 // create and append our fish vote button
 let fishVoteBtn = document.createElement('button');
-fishVoteBtn.textContent = "Vote for Fish";
+fishVoteBtn.textContent = "+ Vote for Fish";
 fishDiv.append(fishVoteBtn);
 
 fishVoteBtn.addEventListener('click', function(){
@@ -101,5 +146,33 @@ fishVoteBtn.addEventListener('click', function(){
     //sets the item in local storage, then we retrieve it AT TOP OF PAGE using getItem. 
     localStorage.setItem('fishVotes', fishVotes);
     
+// adds to total vote column by adding all three together
+    totalVotesTd.textContent = dogVotes + catVotes + fishVotes;
+
 })
 
+
+// create fish vote MINUS button
+let SubFishVoteBtn = document.createElement("button")
+SubFishVoteBtn.textContent = "- Vote for Fish"
+fishDiv.append(SubFishVoteBtn)
+
+SubFishVoteBtn.addEventListener('click', function(e) {
+        fishVotes = fishVotes - 1;
+        fishVotesTd.textContent = fishVotes;
+        localStorage.setItem('fishVotes', fishVotes);
+// makes sure total vote count is also subtracted from
+        totalVotesTd.textContent = dogVotes + catVotes + fishVotes;
+
+});
+
+
+
+
+
+
+
+
+
+
+  
